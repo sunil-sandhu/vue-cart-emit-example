2fa525bcbf637138e4c4dc5625696c38f5bfcdf2
Sunil Sandhu <sunil.sandhu87@gmail.com>
2018-08-29 10:07:51 +0100

    created initial Vue-Cli build and removed bloat

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+
+    <section id="Header">
+      <h1>Fruiticous!</h1>
+
+      <div id="Cart">
+        <p>Cart (0)</p>
+        <button>Empty Cart</button>
+      </div>
+    </section>
+
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'app',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
